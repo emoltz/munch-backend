cb298ce1e030c8cdc1ba632c0468ccaa90a96f01
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-05-29 12:01:31 -0400

    added .env support

--- food_tracker_backend/settings.py
+++ food_tracker_backend/settings.py
@@ -12,20 +12,29 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3d#=dg6*qcsjvnaevgyutpw^)3qzrx#+lr2nry!0t#l)djhq^@'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-3d#=dg6*qcsjvnaevgyutpw^)3qzrx#+lr2nry!0t#l)djhq^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
