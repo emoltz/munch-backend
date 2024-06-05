98b4045c5293d6507c524ab9de991d84ecdb5f36
Ethan Shafran Moltz <esmoltz@gmail.com>
2024-06-05 16:08:07 -0400

    changed URL of meals

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ from api.views import GetTextResponse, GetMealsAndDetails, GetFoodDetails
 urlpatterns = [
     path('get_text_response/', GetTextResponse.as_view(), name='get_text_response'),
     path('token-auth/', obtain_auth_token, name="api_token_auth"),
-    path('meals/', GetMealsAndDetails.as_view(), name='get_meal_totals'),
+    path('meals/', GetMealsAndDetails.as_view(), name='get_meal_info'),
     path('food/<str:id>/', GetFoodDetails.as_view(), name='get_food_details'),
 ]
