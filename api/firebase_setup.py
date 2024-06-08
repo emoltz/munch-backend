09a19b9eb745d74ec1a52dee3fe7115b635706e1
Ethan Shafran Moltz <esmoltz@gmail.com>
2024-06-07 21:33:34 -0400

    adding image capability pt. 3

--- api/firebase_setup.py
+++ api/firebase_setup.py
@@ -1,18 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials, storage
-
+import json
+import base64
 from food_tracker_backend.settings import env
 
 # Path to your service account key file
-service_account_path = env('SERVICE_ACCOUNT_PATH', default='/app/api/serviceAccountKey.json')
+# service_account_path = env('SERVICE_ACCOUNT_PATH', default='/app/api/serviceAccountKey.json')
+encoded_key = env('FIREBASE_SERVICE_ACCOUNT')
 
-cred = credentials.Certificate(service_account_path)
+# Decode the base64 string
+decoded_key = base64.b64decode(encoded_key)
 
-# Initialize the Firebase app
+# Load the key as a JSON object
+service_account_info = json.loads(decoded_key)
+
+# Initialize Firebase app with the decoded service account info
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'munch-f2d84.appspot.com'
 })
-
 
 def upload_image_to_firebase(image_file):
     # Get the default bucket
